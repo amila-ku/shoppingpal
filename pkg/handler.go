c2a2775fdbf5bb63508dd6e0439a2e5d89f794a7
amilaku <amila.kumaranayaka@gmail.com>
2019-08-08 00:26:22 +0200

    fix func name

--- pkg/handler.go
+++ pkg/handler.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func HomePage(w http.ResponseWriter, r *http.Request) {
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
