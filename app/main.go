547d8f343439ccfdf860e3d8768168eb5134c979
amilaku <amila.kumaranayaka@gmail.com>
2019-08-08 00:21:20 +0200

    update structure

---
+++ app/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+        "github.com/amila-ku/shoppingpal/pkg"
+
+)
+
+func main() {
+
+	fmt.Println("starting")
+	handleRequests()
+
+}
--- pkg/handler.go
+++ pkg/handler.go
@@ -19,19 +19,30 @@ func handleRequests() {
 
 func returnAllItems(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllItems")
+	source := Source{
+		Name:          "N1",
+		Author:        "A1",
+		Title:         "T1",
+		Content:       "C1",
+		URL:           "https://test",
+		DatePublished: "09-08-2019",
+	}
+
 	item1 := Item{
-		Name:  "Book",
-		Price: "$20",
-		Title: "My Book",
+		Name:    "Book",
+		Price:   40,
+		Summary: "My Book",
+		Sources: source,
 	}
 
 	item2 := Item{
-		Name:  "Book2",
-		Price: "$20",
-		Title: "My Book 2",
+		Name:    "Book2",
+		Price:   20,
+		Summary: "My Book 2",
+		Sources: source,
 	}
 
-	ItemList := []Items{
+	ItemList := Items{
 		item1,
 		item2,
 	}
